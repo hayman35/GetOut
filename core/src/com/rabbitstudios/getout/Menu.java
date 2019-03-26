78045fe7b6e097d6b84a5826e45d07f98c44576d
Hayman Ati <hayman-35@hotmail.com>
2019-03-26 14:18:05 -0400

    Testing

--- core/src/com/rabbitstudios/getout/Menu.java
+++ core/src/com/rabbitstudios/getout/Menu.java
@@ -1,13 +1,12 @@
 package com.rabbitstudios.getout;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class Menu extends Game {
+public class Menu extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
 	
