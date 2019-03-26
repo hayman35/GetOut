64e16d17dad3034daf54d4060d74771a2f24ad84
Hayman Ati <hayman-35@hotmail.com>
2019-03-26 14:15:51 -0400

    Start of the game

---
+++ android/src/com/rabbitstudios/getout/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.rabbitstudios.getout;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.rabbitstudios.getout.Menu;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Menu(), config);
+	}
+}
