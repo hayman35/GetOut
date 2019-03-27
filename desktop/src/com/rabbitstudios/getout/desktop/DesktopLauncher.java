6bac89f8ce1ceb40ad51f897563317f39be41cde
Hayman Ati <hayman-35@hotmail.com>
2019-03-27 13:51:08 -0400

    fixed the black bars at the top and bottom of the screen

--- desktop/src/com/rabbitstudios/getout/desktop/DesktopLauncher.java
+++ desktop/src/com/rabbitstudios/getout/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import com.rabbitstudios.getout.GetOut;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1200;
+		config.height = 624;
 		new LwjglApplication(new GetOut(), config);
 	}
 }
